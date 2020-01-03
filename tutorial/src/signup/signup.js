8afe3ffce40dadad4ef243ad63911e8485f5a950
Tanzania Coleman <tanzania.coleman@gmail.com>
2020-01-03 10:40:27 -0500

    signup content is loaded on app

--- tutorial/src/signup/signup.js
+++ tutorial/src/signup/signup.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import styles from './styles'
 
-class SignUpComponent extends React.Component{
+
+const firebase = require('firebase')
+class SignUpComponent extends React.Component {
 
     render() {
-        return(
-            <div> Hello World from SignUp
-            </div>
+
+        const { classes } = this.props;
+        return (
+            <main className={classes.main}>
+                <CssBaseline>
+                </CssBaseline>
+                <Paper className={classes.paper}>
+                    <Typography component='h1' variant='h5'>
+                        SignUp!
+          </Typography>
+                    <form onSubmit={(e) => this.submitSignup(e)} className={classes.form}>
+                        <FormControl required fullWidth margin='normal'>
+                            <InputLabel htmlFor="signup-email-input"> Enter your email addres</InputLabel>
+                            <Input autoComplete='email' onChange={(e)=> this.userTyping('email', e)}autoFocus id='signup-email-input'></Input>
+                            </FormControl>
+                    </form>
+                </Paper>
+            </main>
         )
+    }
+
+userTyping = (e) =>{
+    console.log('Typed')
+}
+    submitSignup = (e) => {
+        console.log('submitting')
     }
 }
 
-export default SignUpComponent
+export default withStyles(styles)(SignUpComponent); //adding all of js styles as a property to sign up component
