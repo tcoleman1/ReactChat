44f72d2b765ad9987ef14c785d4dd4ca73f29ad7
Tanzania Coleman <tanzania.coleman@gmail.com>
2020-01-03 11:54:57 -0500

    imported link to route to signup if acct exists, added state and turnary for signup error.. am able to see state updating in console whenever form is submitted

--- tutorial/src/signup/signup.js
+++ tutorial/src/signup/signup.js
@@ -8,11 +8,25 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import styles from './styles'
+import { Link } from 'react-router-dom';
+
 
 
 const firebase = require('firebase')
 class SignUpComponent extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+
+            email: "null",
+            password: "null",
+            passwordConfirmation: "null",
+            signupError: '',
+
+
+        }
+    }
     render() {
 
         const { classes } = this.props;
@@ -27,32 +41,59 @@ class SignUpComponent extends React.Component {
                     <form onSubmit={(e) => this.submitSignup(e)} className={classes.form}>
                         <FormControl required fullWidth margin='normal'>
                             <InputLabel htmlFor="signup-email-input"> Enter your email addres</InputLabel>
-                            <Input autoComplete='email' onChange={(e)=> this.userTyping('email', e)} autoFocus id='signup-email-input'></Input>
-                            </FormControl>
-                            <FormControl required fullWidth margin='normal'>
+                            <Input autoComplete='email' onChange={(e) => this.userTyping('email', e)} autoFocus id='signup-email-input'></Input>
+                        </FormControl>
+                        <FormControl required fullWidth margin='normal'>
                             <InputLabel htmlFor='signup-password-input'> Create Password</InputLabel>
                             <Input type='password' onChange={(e) => this.userTyping('password', e)} autoFocus id='signup-password-input'></Input>
-                            </FormControl>
-                            <FormControl required fullWidth margin='normal'>
+                        </FormControl>
+                        <FormControl required fullWidth margin='normal'>
                             <InputLabel htmlFor='signup-password-confirmation-input'> Confirm Your Password</InputLabel>
                             <Input type='password' onChange={(e) => this.userTyping('passwordConfirmation', e)} autoFocus id='signup-password-confirmation-input'></Input>
-                            </FormControl>
+                        </FormControl>
 
-                            <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}> Submit
+                        <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}> Submit
                             </Button>
 
-                
+
                     </form>
+                    {
+                        this.state.signupError ?
+                            <Typography className={classes.errorText} component='h5' variant='h6'>
+                                {this.state.signupError}
+                            </Typography> : null
+                    }
+
+                    <Typography component='h5' variant='h6' className={classes.hasAccountHeader}> Already Have An Account?</Typography>
+                    <Link className={classes.logInLink} to="/login"> LOGIN </Link>
                 </Paper>
             </main>
         )
     }
 
-userTyping = (type, e) => {
-    console.log(type,e)
-}
+    userTyping = (type, e) => {
+       switch(type){
+           case 'email':
+           this.setState({ email: e.target.value })
+           break;
+
+           case 'password':
+               this.setState({ password: e.target.value })
+               break;
+
+            case 'passwordConfirmation':
+                this.setState({ passwordConfirmation: e.target.value })
+                break;
+
+                default:
+                    break;
+       }
+
+    //    console.log('this is the email:', this.state.email)
+    }
     submitSignup = (e) => {
-        console.log('submitting')
+        e.preventDefault();
+        console.log('submitting', this.state)
     }
 }
 
